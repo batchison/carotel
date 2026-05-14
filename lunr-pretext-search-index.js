6853fab2945643de4f035672150e515b379661c5
batchison <batchison@framingham.edu>
2026-05-14 10:04:44 -0400

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -61,7 +61,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "",
   "title": "Statistics for the Natural Sciences: Statistical Thinking",
-  "body": " Statistics for the Natural Sciences: Statistical Thinking   This is an assignment for an introductory statistics course intendend for students pursuing a degree in the natural sciences.    Objectives  This activity highlights the statistical thinking mindset, where we observe several ways to quantitatively summarize data and then draw justified conclusions about a population.    Data Description  The below table reports the number of people who became very sick after contracting an illness, broken out by if they took a particular drug. We will refer to those who took the drug as the treatment group, and those who did not take the drug as the control group. Note that the drug was not randomly assigned in this setting. Does this drug work?   Drug Treatment and Sickness     Control  Treatment  Overall    Sick  253  402  655    Total  151,054  47,008  198,062       Calculate the proportions of the control group and the treatment group who became sick, and compare the proportions. Which group had a higher rate of sickness?      What other factors may we want to consider before concluding that the drug makes you more likely to get very sick after contracting the illness?      Including More Data  One additional factor we have access to is the age (younger or older) of the people. Below is a table that includes the data disaggregated by age group.   Drug Treatment, Age, and Sickness      Control   Treatment   Overall       Younger  Older  Younger  Older  Younger  Older     Sick  48  205  13  389  61  594     Total  147,612  3,442  25,536  21,472  173,148  24,912       Explore the data in Table 2 by computing several more proportions. In particular, compute the sickness rates for: (i) the younger control people, (ii) the younger treatment people, (iii) the older control people, and (iv) the older treated people.      Compute the proportion of older people in the control group and older people in the treated group      What do you notice about the proportions you just calculated? Do these results really show that the drug does not work?      NACE Competencies  This assignment fulfills NACE competencies XXX (for more information on NACE competencies, please see the document titled 'NACE Competencies').    This assignment was created by Njesa Totty for the CA-ROTEL PreTeXt Working Group at Framingham State University. It is licensed under a BY-NC-SA Creative Commons license.   CC BY-NC-SA    "
+  "body": " Statistics for the Natural Sciences: Statistical Thinking   This is an assignment for an introductory statistics course intendend for students pursuing a degree in the natural sciences.    Objectives  This activity highlights the statistical thinking mindset, where we observe several ways to quantitatively summarize data and then draw justified conclusions about a population.    Data Description  The below table reports the number of people who became very sick after contracting an illness, broken out by if they took a particular drug. We will refer to those who took the drug as the treatment group, and those who did not take the drug as the control group. Note that the drug was not randomly assigned in this setting. Does this drug work?       Control  Treatment  Overall    Sick  253  402  655    Total  151,054  47,008  198,062       Calculate the proportions of the control group and the treatment group who became sick, and compare the proportions. Which group had a higher rate of sickness?      What other factors may we want to consider before concluding that the drug makes you more likely to get very sick after contracting the illness?      Including More Data  One additional factor we have access to is the age (younger or older) of the people. Below is a table that includes the data disaggregated by age group.        Control   Treatment   Overall       Younger  Older  Younger  Older  Younger  Older     Sick  48  205  13  389  61  594     Total  147,612  3,442  25,536  21,472  173,148  24,912       Explore the data in Table 2 by computing several more proportions. In particular, compute the sickness rates for: (i) the younger control people, (ii) the younger treatment people, (iii) the older control people, and (iv) the older treated people.      Compute the proportion of older people in the control group and older people in the treated group      What do you notice about the proportions you just calculated? Do these results really show that the drug does not work?      NACE Competencies  This assignment fulfills NACE competencies 2,3, and 7 (for more information on NACE competencies, please see the document titled 'NACE Competencies').    This assignment was created by Njesa Totty for the CA-ROTEL PreTeXt Working Group at Framingham State University. It is licensed under a BY-NC-SA Creative Commons license.   CC BY-NC-SA    "
 },
 {
   "id": "table-drug-data",
@@ -69,14 +69,14 @@ var ptx_lunr_docs = [
   "url": "statistical-thinking-activity.html#table-drug-data",
   "type": "Table",
   "number": "1",
-  "title": "Drug Treatment and Sickness",
-  "body": " Drug Treatment and Sickness     Control  Treatment  Overall    Sick  253  402  655    Total  151,054  47,008  198,062    "
+  "title": "",
+  "body": "     Control  Treatment  Overall    Sick  253  402  655    Total  151,054  47,008  198,062    "
 },
 {
   "id": "sec-data-description-4",
   "level": "2",
   "url": "statistical-thinking-activity.html#sec-data-description-4",
-  "type": "Question",
+  "type": "Checkpoint",
   "number": "2",
   "title": "",
   "body": "  Calculate the proportions of the control group and the treatment group who became sick, and compare the proportions. Which group had a higher rate of sickness?   "
@@ -85,7 +85,7 @@ var ptx_lunr_docs = [
   "id": "sec-data-description-5",
   "level": "2",
   "url": "statistical-thinking-activity.html#sec-data-description-5",
-  "type": "Question",
+  "type": "Checkpoint",
   "number": "3",
   "title": "",
   "body": "  What other factors may we want to consider before concluding that the drug makes you more likely to get very sick after contracting the illness?   "
@@ -96,14 +96,14 @@ var ptx_lunr_docs = [
   "url": "statistical-thinking-activity.html#table-age-drug",
   "type": "Table",
   "number": "4",
-  "title": "Drug Treatment, Age, and Sickness",
-  "body": " Drug Treatment, Age, and Sickness      Control   Treatment   Overall       Younger  Older  Younger  Older  Younger  Older     Sick  48  205  13  389  61  594     Total  147,612  3,442  25,536  21,472  173,148  24,912    "
+  "title": "",
+  "body": "      Control   Treatment   Overall       Younger  Older  Younger  Older  Younger  Older     Sick  48  205  13  389  61  594     Total  147,612  3,442  25,536  21,472  173,148  24,912    "
 },
 {
   "id": "sec-including-more-data-4",
   "level": "2",
   "url": "statistical-thinking-activity.html#sec-including-more-data-4",
-  "type": "Question",
+  "type": "Checkpoint",
   "number": "5",
   "title": "",
   "body": "  Explore the data in Table 2 by computing several more proportions. In particular, compute the sickness rates for: (i) the younger control people, (ii) the younger treatment people, (iii) the older control people, and (iv) the older treated people.   "
@@ -112,7 +112,7 @@ var ptx_lunr_docs = [
   "id": "sec-including-more-data-5",
   "level": "2",
   "url": "statistical-thinking-activity.html#sec-including-more-data-5",
-  "type": "Question",
+  "type": "Checkpoint",
   "number": "6",
   "title": "",
   "body": "  Compute the proportion of older people in the control group and older people in the treated group   "
@@ -121,7 +121,7 @@ var ptx_lunr_docs = [
   "id": "sec-including-more-data-6",
   "level": "2",
   "url": "statistical-thinking-activity.html#sec-including-more-data-6",
-  "type": "Question",
+  "type": "Checkpoint",
   "number": "7",
   "title": "",
   "body": "  What do you notice about the proportions you just calculated? Do these results really show that the drug does not work?   "
